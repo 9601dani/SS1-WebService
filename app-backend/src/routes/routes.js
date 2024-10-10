d112827fb70bb4918dbf4e1587550e5e6ee0f33c
9601dani <erickmorales201930699@cunoc.edu.gt>
2024-10-09 22:59:01 -0600

    config db

--- app-backend/src/routes/routes.js
+++ app-backend/src/routes/routes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();
-
+const getConnection = require('../../db/db');
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get("", (req, res) => {
   res.send("Hello World!");
+});
+
+router.get("/test-db", async (req, res) => {
+  try{
+    const connection = await getConnection();
+    connection.release();
+    res.send("Connection to database was successful.");
+  } catch (error) {
+    res.status(500).send("Connection to database failed.");
+  }
 });
 
 module.exports = router;
