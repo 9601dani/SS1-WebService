b7d4c9fc9df8995de5b5b0c4740899e674e5f6bc
9601dani <erickmorales201930699@cunoc.edu.gt>
2024-10-29 22:47:04 -0600

    hotfix ..

--- app-backend/src/routes/api.routes.js
+++ app-backend/src/routes/api.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const middleware = require('../../middleware/verifytoken.middleware');
 const apiController = require('../controllers/api.controller');
 
-router.get('/auth/token', apiController.getToken);
-router.get('/card',[middleware.verifyTokenApi], apiController.getCard);
+router.post('/auth/token', apiController.getToken);
+router.post('/card',[middleware.verifyTokenApi], apiController.getCard);
 router.post('/pay',[middleware.verifyTokenApi], apiController.pay);
 
 module.exports = router;
